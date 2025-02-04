6b6886a41ca94de01c16f7e2297f9b38b39d1791
erick <erickestradarosario@gmail.com>
2025-02-03 20:24:39 -0400

    Crea Services

---
+++ Erick_Estrada_Rosario_P1_AP1/Services/Services.cs
@@ -0,0 +1,47 @@
+using System.Linq.Expressions;
+using Erick_Estrada_P1_AP1.Models;
+
+namespace Erick_Estrada_P1_AP1.Services;
+
+public class Services ()
+{
+    
+    public async Task<bool> Guardar(Modelo modelo)
+    {
+            return true;
+        
+    }
+    
+    
+     public async Task<bool> Existe(int Id)
+     {
+         return true;
+     }
+     
+    
+    public async Task<bool> Insertar(Modelo modelo)
+    {
+        return true;
+    }
+
+    public async Task<bool> Eliminar(int id)
+    {
+        return true;
+    }
+
+    public async Task<bool> Modificar(Modelo modelo)
+    {
+        return true;
+    }
+    
+    
+    public async Task<Modelo?> Buscar(int id)
+    {
+        return new Modelo();
+    }
+
+    public async Task<List<Modelo>> Listar(Expression<Func<Modelo, bool>>criterio)
+    {
+        return new List<Modelo>();
+    }
+}
