01195f81ce74356af23d3010c2b7b1ccddc5723b
erick <erickestradarosario@gmail.com>
2025-02-17 18:53:29 -0400

    Crea el modelo aportes

---
+++ Erick_Estrada_Rosario_P1_AP1/Models/Aportes.cs
@@ -0,0 +1,21 @@
+using System.ComponentModel.DataAnnotations;
+namespace Erick_Estrada_Rosario_P1_AP1.Models;
+
+public class Aportes
+{
+    [Key]
+    public int AporteId   { get; set; }
+    
+    [Required(ErrorMessage = "Este campo es requerido")]
+    public string NombrePersona { get; set; }
+    
+    [Required(ErrorMessage = "Este campo es requerido")]
+    [MaxLength(250)]
+    public string Observacion { get; set; }
+    
+    [Required (ErrorMessage = "Este campo es requerido")]
+    [Range(0,int.MaxValue)]
+    public double Monto { get; set; }
+    
+}
+
--- Erick_Estrada_Rosario_P1_AP1/Models/Modelo.cs
+++
@@ -1,8 +0,0 @@
-using System.ComponentModel.DataAnnotations;
-namespace Erick_Estrada_Rosario_P1_AP1.Models;
-
-public class Modelo
-{
-    [Key]
-    public int Id   { get; set; }
-}
