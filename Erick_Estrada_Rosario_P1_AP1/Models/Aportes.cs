e88c8be2da1e35bcf71d6721bf00214749faeeed
erick <erickestradarosario@gmail.com>
2025-02-17 20:02:09 -0400

    Añadi restricciones al modelo

--- Erick_Estrada_Rosario_P1_AP1/Models/Aportes.cs
+++ Erick_Estrada_Rosario_P1_AP1/Models/Aportes.cs
@@ -1,4 +1,6 @@
 using System.ComponentModel.DataAnnotations;
+using Erick_Estrada_Rosario_P1_AP1.Components.Pages;
+
 namespace Erick_Estrada_Rosario_P1_AP1.Models;
 
 public class Aportes
@@ -7,15 +9,15 @@ public class Aportes
     public int AporteId   { get; set; }
     
     [Required(ErrorMessage = "Este campo es requerido")]
-    [MaxLength(50)]
+    [MaxLength(50,ErrorMessage = "El limite de caracteres es 50")]
     public string NombrePersona { get; set; }
     
     [Required(ErrorMessage = "Este campo es requerido")]
-    [MaxLength(250)]
+    [MaxLength(250,ErrorMessage = "El limite de Caracteres es 250")]
     public string Observacion { get; set; }
     
     [Required (ErrorMessage = "Este campo es requerido")]
-    [Range(0,int.MaxValue)]
+    [Range(0,int.MaxValue,ErrorMessage = "Valor incorrecto.")]
     public double Monto { get; set; }
     
     [Required]
